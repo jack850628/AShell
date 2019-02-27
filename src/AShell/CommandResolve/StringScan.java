327686dd63242a921603e41a47764ede52f9218e
奇幻 <30308931+jack850628@users.noreply.github.com>
2019-02-27 20:46:20 +0800

    修正在指令交互模式下遇到#註解會卡死的問題

--- src/AShell/CommandResolve/StringScan.java
+++ src/AShell/CommandResolve/StringScan.java
@@ -107,7 +107,8 @@ public class StringScan {
                 String str=command_str.substring(command_char_index, command_char_index+1);
                 //System.out.print(str);
                     if(str.equals("#")&&!Annotation){
-                            break;
+                        command_char_index = str.length();
+                        break;
                     }else if(str.equals("/")&&!Annotation){
                         if(command_char_index+1<command_str.length()&&command_str.substring(command_char_index+1, command_char_index+2).equals("*")){
                             command_char_index++;
