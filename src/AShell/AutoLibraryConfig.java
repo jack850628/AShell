4cf86413057891efe849a01858e76a9842caa73a
奇幻 <30308931+jack850628@users.noreply.github.com>
2020-04-06 00:00:48 +0800

    加強運算子(** - +)的運算效率

--- src/AShell/AutoLibraryConfig.java
+++ src/AShell/AutoLibraryConfig.java
@@ -3,6 +3,6 @@ package AShell;
  * 自動載入函示庫設定
  */
 public class AutoLibraryConfig {
-    public final static String LIBRARY_NAME="AotoLoadLibrary";
-    public final static String LIBRARY_NAME_FOR_INTRACTIVE_MODE="AotoLoadLibraryForInteractiveMode";
+    public final static String LIBRARY_NAME="AutoLoadLibrary";
+    public final static String LIBRARY_NAME_FOR_INTRACTIVE_MODE="AutoLoadLibraryForInteractiveMode";
 }
--- src/using_file/AutoLoadLibraryForInteractiveMode.ash
+++ src/using_file/AutoLoadLibraryForInteractiveMode.ash
@@ -1,4 +1,4 @@
-using AotoLoadLibrary
+using AutoLoadLibrary
 
 native help("AShell.Native_Class.AShell_InteractiveModeFunction")
 native exit("AShell.Native_Class.AShell_InteractiveModeFunction")
